46c02492b01d8207f3c0fb8a2105ac46c03a15a1
Icaro Silva <107263783+Icsilva01@users.noreply.github.com>
2024-03-24 13:09:44 -0300

    chore:add new faker dependence

--- src/data/usecases/autentication/remote-autentication.spec.ts
+++ src/data/usecases/autentication/remote-autentication.spec.ts
@@ -1,14 +1,15 @@
-import { HttpPostClientSpy } from "../../test/mock-http-client";
-import { RemoteAutentication } from "./remote-autentication";
+import { HttpPostClientSpy } from "../../test/mock-http-client"
+import { RemoteAutentication } from "./remote-autentication"
+import { faker } from '@faker-js/faker'
 
 type SutTypes = {
   sut:RemoteAutentication
   httpPostClientSpy: HttpPostClientSpy
 }
 
-const makeSut = (url: string = 'any_url'): SutTypes => {
-  const httpPostClientSpy = new HttpPostClientSpy();
-  const sut = new RemoteAutentication(url, httpPostClientSpy);
+const makeSut = (url: string = faker.internet.url()): SutTypes => {
+  const httpPostClientSpy = new HttpPostClientSpy()
+  const sut = new RemoteAutentication(url, httpPostClientSpy)
   return{
     sut,
     httpPostClientSpy
@@ -17,9 +18,9 @@ const makeSut = (url: string = 'any_url'): SutTypes => {
 
 describe("RemoteAutentication", () => {
   test("Should call HttpPostClient with correct URL", async () => {
-    const url = "other_url";
-    const { sut, httpPostClientSpy } = makeSut(url);
-    await sut.auth();
-    expect(httpPostClientSpy.url).toBe(url);
-  });
-});
+    const url = faker.internet.url()
+    const { sut, httpPostClientSpy } = makeSut(url)
+    await sut.auth()
+    expect(httpPostClientSpy.url).toBe(url)
+  })
+})
