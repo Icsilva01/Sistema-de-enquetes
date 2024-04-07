516407876e7c3aa643ebf6cfcc90eeb83c099c85
Icaro Silva <107263783+Icsilva01@users.noreply.github.com>
2024-04-07 11:38:00 -0300

    test: intrgration with authentication in Login

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,3 +1,4 @@
+import { Autentication } from "@/domain/usecases";
 import {
   BackgroundImg,
   FormStatus,
@@ -5,16 +6,17 @@ import {
   LoginFooter,
   LoginHeader,
 } from "@/presentation/components";
-import React, { useState, useEffect } from "react";
-import Styles from "./login-styles.scss";
 import formContext from "@/presentation/components/contexts/form/form-context";
 import { Validation } from "@/presentation/protocols/validation";
+import React, { useEffect, useState } from "react";
+import Styles from "./login-styles.scss";
 
 type Props = {
   validation: Validation;
+  authentication: Autentication
 };
 
-const Login: React.FC<Props> = ({ validation }: Props) => {
+const Login: React.FC<Props> = ({ validation, authentication }: Props) => {
   const [state, setState] = useState({
     isLoading: false,
     email: "",
@@ -32,12 +34,21 @@ const Login: React.FC<Props> = ({ validation }: Props) => {
     });
   }, [state.email, state.password]);
 
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement> ):Promise<void> => {
+    event.preventDefault()
+    setState({...state, isLoading: true})
+    await authentication.auth({
+      email:state.email,
+      password:state.password
+    })
+  }
+
   return (
     <div className={Styles.login}>
       <LoginHeader />
       <formContext.Provider value={{ state, setState }}>
         <div className={Styles.boxForm}>
-          <form className={Styles.form}>
+          <form className={Styles.form} onSubmit={handleSubmit}>
             <h2>Bem vindo a sua plataforma de enquetes!</h2>
             <p>E-mail</p>
             <Input type="email" name="email" placeholder="Digite seu e-mail" />
@@ -45,7 +56,7 @@ const Login: React.FC<Props> = ({ validation }: Props) => {
             <Input type="password" name="password" placeholder="Digite sua senha" />
             <button
               data-testid="submit"
-              disabled
+              disabled={!!state.emailError || !!state.passwordError}
               className={Styles.submit}
               type="submit"
             >
