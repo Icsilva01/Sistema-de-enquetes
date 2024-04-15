28000ac56cf366c7eb420a50c22bde978c48f4f6
Icaro Silva <107263783+Icsilva01@users.noreply.github.com>
2024-04-15 16:11:35 -0300

    fix: package-lock.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const { DefinePlugin } = require('webpack')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
@@ -48,8 +49,8 @@ module.exports = {
   },
   plugins: [
     new CleanWebpackPlugin(),
-    // new DefinePlugin({
-    //   'process.env.API_URL': 'http://fordevs.herokuapp.com/api'
-    // })
+    new DefinePlugin({
+      'process.env.API_URL': 'http://fordevs.herokuapp.com/api'
+    })
   ]
 }
